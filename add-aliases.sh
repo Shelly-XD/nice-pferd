9f7e6587dfe82e3b9034b06d24ddeb63a873960f
augustin64 <augustin.lucas64@gmail.com>
2021-04-18 19:28:14 +0200

    modifié :         add-aliases.sh

--- add-aliases.sh
+++ add-aliases.sh
@@ -1,4 +1,4 @@
 SCRIPT=$(readlink -f "$0")
 NICE_PFERD_PATH=$(dirname "$SCRIPT")
 echo "alias nice-pferd='cd "$NICE_PFERD_PATH" && flask run'" >> ~/.bashrc
-echo "alias nice-pferd-upgrade='rm -rf "$NICE_PFERD_PATH" && cd "$NICE_PFERD_PATH/.." && git clone https://github.com/augustin64/nice-pferd'"
+echo "alias nice-pferd-upgrade='rm -rf "$NICE_PFERD_PATH" && cd "$NICE_PFERD_PATH/.." && git clone https://github.com/augustin64/nice-pferd'" >> .bashrc
