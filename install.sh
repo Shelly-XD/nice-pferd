9dadf0f90073a15ce2f7b212b5de246de903a1c2
augustin64 <augustin.lucas64@gmail.com>
2021-04-18 22:00:12 +0200

    added comments

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
+#get nice-pferd location
 SCRIPT=$(readlink -f "$0")
 NICE_PFERD_PATH=$(dirname "$SCRIPT")
-
+#install python dependencies
 pip install -r "$NICE_PFERD_PATH/requirements.txt"
-
+#add aliases
 echo "alias nice-pferd='cd "$NICE_PFERD_PATH" && flask run'" >> ~/.bashrc
 echo "alias nice-pferd-upgrade='cd "$NICE_PFERD_PATH"/.. && rm -rf "$NICE_PFERD_PATH" && git clone https://github.com/augustin64/nice-pferd'" >> ~/.bashrc
-
+#make ~/.bashrc executable and execute it to get all the brand new aliases
 chmod +x ~/.bashrc && ~/.bashrc
-
-echo -e '\nUse nice-pferd to start the local server and then use a browser and go to http://localhost:5000\nUse nice-pferd-upgrade to upgrade to the latest version available'
+#Display a help message
+echo -e '\nUse nice-pferd to start the local server and then use a browser and go to http://localhost:5000 to have a GUI\nUse nice-pferd-upgrade to upgrade to the latest version available'
