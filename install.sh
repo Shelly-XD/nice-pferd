f41e61fbf8c67bf1beba48fd0ba5bcb6275e90fd
augustin64 <augustin.lucas64@gmail.com>
2021-04-19 18:55:48 +0200

    Modified installation script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 SCRIPT=$(readlink -f "$0")
 NICE_PFERD_PATH=$(dirname "$SCRIPT")
 #install python dependencies
-pip install -r "$NICE_PFERD_PATH/requirements.txt"
+python3 -m pip install -r "$NICE_PFERD_PATH/requirements.txt"
 #add aliases
 echo "alias nice-pferd='cd "$NICE_PFERD_PATH" && flask run'" >> ~/.bashrc
 echo "alias nice-pferd-upgrade='cd "$NICE_PFERD_PATH"/.. && rm -rf "$NICE_PFERD_PATH" && git clone https://github.com/augustin64/nice-pferd'" >> ~/.bashrc
